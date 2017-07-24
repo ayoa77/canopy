d29455ae77f69c668778dece9514638d58e16e1b
ayoa77 <ayodeleamadi@gmail.com>
2017-07-24 15:40:33 +0800

    pushing menu layover

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -14,5 +14,6 @@ Spree.config do |config|
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
 end
+Spree::Auth::Config[:confirmable] = true
 
 Spree.user_class = "Spree::User"
