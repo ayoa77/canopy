aed90d5ae989c7daf6704bb367f63b8de2ef12b7
ayoa77 <ayodeleamadi@gmail.com>
2017-07-27 10:18:42 +0800

    adding files to be editable

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,28 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+  default from: 'boxchoctw@gmail.com'
+
+  def comment_email(name, email, message)
+    @name = name
+    @email = email
+    @message = message
+    @time = Time.now
+   mail(to: 'ayodeleamadi@gmail.com', subject: "Comment or question from #{@name}, #{@email}") do |format|
+     format.html { render layout: 'comment_email' }
+   end
+ end
+
+  def reserve_email(name, email, message, room, start, finish,nationality, phone)
+    @name = name
+    @email = email
+    @phone = phone
+    @nationality = nationality
+    @room = room
+    @message = message
+    @start = start
+    @finish = finish
+    @time = Time.now
+   mail(to: 'ayodeleamadi@gmail.com', subject: "Reservation from #{@name}, #{@email}") do |format|
+     format.html { render layout: 'reserve_email' }
+   end
+end
 end
