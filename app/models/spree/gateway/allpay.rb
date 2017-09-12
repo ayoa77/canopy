d8d36d071090c878d65a397907ca69a7d195000b
ayoa77 <ayodeleamadi@gmail.com>
2017-09-12 14:55:35 +0800

    adding attempts to use allpay

--- app/models/spree/gateway/allpay.rb
+++ app/models/spree/gateway/allpay.rb
@@ -2,9 +2,26 @@ module Spree
   class Gateway::Allpay < Gateway
     preference :account, :string
     preference :password, :string
+    preference :merchant_id, :string
+    preference :hash_key, :string
+    preference :hash_iv, :string
+    preference :server, :select, default: -> { { values: [:test, :production] } }
+    preference :pass_billing_and_shipping_address, :boolean_select, default: false
+    preference :return_url, :string
+    preference :notify_url, :boolean_select, default: false
+    preference :descriptor_name, :string
+    preference :currency_merchant_accounts, :hash, default: {}
 
     def provider_class
-        OffsitePayments::Integrations::Allpay::Helper
+        OffsitePayments::Integrations::Allpay
+    end
+
+    def provider_class_helper
+      provider_class::Helper
+    end
+
+    def purchase(amount, transaction_details, gateway_options={} )
+      OffsitePayments::Integrations::Allpay::Notification.new
     end
   end
 end
