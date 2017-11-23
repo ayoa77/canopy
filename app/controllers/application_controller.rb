522a48b009da5234d5168ae5eedbe1af0a6cf73a
ayoa77 <ayodeleamadi@gmail.com>
2017-11-23 15:56:49 +0800

    pushing error pages and controller changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ rescue_from ActionController::RoutingError, :with => :rescue404
 
 # $addon_price = Spree::Product.find_by(description2: "addon").price || 20
 #
-#  def rescue404
-#    #your custom method for errors, you can render anything you want there
-#  end
+  def rescue404
+    render 'public/404'
+  end
+
 end
