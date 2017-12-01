9fcfa118035b5975d0b03c2bcbc964643772d0e1
ayoa77 <ayodeleamadi@gmail.com>
2017-12-01 17:15:39 +0800

    pushing clean up

--- app/models/spree/custom.rb
+++ app/models/spree/custom.rb
@@ -1,28 +1,28 @@
 module Spree
   class Custom < Spree::Base
     has_attached_file :photo1,
-        styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' }
+        styles: { product: '240x240>', large: '600x600>' }
 
     has_attached_file :photo2,
-        styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' }
+        styles: { product: '240x240>', large: '600x600>' }
 
     has_attached_file :photo3,
-        styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' }
+        styles: { product: '240x240>', large: '600x600>' }
 
     has_attached_file :photo4,
-        styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' }
+        styles: { product: '240x240>', large: '600x600>' }
 
     has_attached_file :photo5,
-        styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' }
+        styles: { product: '240x240>', large: '600x600>' }
 
     has_attached_file :photo6,
-        styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' }
+        styles: { product: '240x240>', large: '600x600>' }
 
     has_attached_file :photo7,
-        styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' }
+        styles: { product: '240x240>', large: '600x600>' }
 
     has_attached_file :photo8,
-        styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' }
+        styles: { product: '240x240>', large: '600x600>' }
 
 
       validates_attachment_content_type(
